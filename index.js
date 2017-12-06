65c76c7a381a08778cba55149acf81892614b2c3
Richie Carmichael <richie.carmichael@gmail.com>
2017-12-05 18:43:51 -0800

    update

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-﻿require([
-    'esri/Map',
-    'esri/views/SceneView',
-    'dojo/domReady!'
-], function (
-    Map,
-    SceneView
-) {
+﻿/*
+    Copyright 2017 Esri
+
+    Licensed under the Apache License, Version 2.0 (the 'License');
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at:
+    https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an 'AS IS' BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/*
+    REFERENCE MATERIAL
+
+    The gamepad specification and compatiablity.
+    https://developer.mozilla.org/en-US/docs/Web/API/Gamepad
+
+    Sample application.
+    http://luser.github.io/gamepadtest/
+
+    Source code to sample application.
+    https://github.com/luser/gamepadtest
+*/
+
+require(
+    [
+        'esri/Map',
+        'esri/views/SceneView',
+        'dojo/domReady!'
+    ],
+    function (
+        Map,
+        SceneView
+    ) {
         // Enforce strict mode
         'use strict';
 
@@ -29,37 +59,61 @@
             }
         });
         view.then(function () {
-            gameloop();
+            // When the map loads immediately start the game loop.
+            window.requestAnimationFrame(gameloop);
         });
 
+        // These constants dictate the speed of angular and linear motion. Adjust as necessary.
+        var ANGULAR_RATIO = 4;
+        var LINEAR_RATIO = 0.05;
+
+        // Use this variable to store the at-rest values of the joysticks. Older controllers may not be zero.
         var origin = null;
 
-        function gameloop() {
-            window.requestAnimationFrame(gameloop);
-
-            var gamepads = navigator.getGamepads();
-            if (!gamepads || gamepads.length === 0) {
-                // skip
+        // Parabolic function that will honor the original cardinality. This function is used to de-sensitize the xbox axes.
+        function parabolic(e) {
+            var p = e * e;
+            if (e < 0) {
+                p *= -1;
             }
-            else {
-                var gamepad = gamepads[0];
+            return p;
+        }
+
+        // This function is called every frame.
+        function gameloop() {
+            // Get all attached gamepads. Do not proceed if none exist.
+            var gamepads = navigator.getGamepads();
+            if (gamepads && gamepads.length > 0 && gamepads[0]) {
+                // For simplicity only use first connected gamepad. Assume xbox 360/one controller.
+                var xbox = gamepads[0];
+
+                // Get Esri camera.
                 var camera = view.camera;
 
+                // If this is the first loop then store the at-rest positions of the axis.
                 if (!origin) {
-                    origin = gamepad.axes.slice()
+                    origin = xbox.axes.slice();
                 }
-                var lx = gamepad.axes[0] - origin[0];
-                var ly = gamepad.axes[1] - origin[1];
-                var rx = gamepad.axes[2] - origin[2];
-                var ry = gamepad.axes[3] - origin[3];
-                var lp = gamepad.buttons[4].pressed;
-                var rp = gamepad.buttons[5].pressed;
 
-                var heading = camera.heading + rx;
-                var tilt = camera.tilt - ry;
+                // Get the position of the two xbox axes, apply the origin correction and parabolic curve.
+                // The parabolic function will make movements near the origin less pronounced than the edges.
+                var lx = parabolic(xbox.axes[0] - origin[0]);
+                var ly = parabolic(xbox.axes[1] - origin[1]);
+                var rx = parabolic(xbox.axes[2] - origin[2]);
+                var ry = parabolic(xbox.axes[3] - origin[3]);
 
-                var speed = camera.position.z / 50;
+                // Values for the left and right triggers.
+                var lt = parabolic(xbox.buttons[6].value);
+                var rt = parabolic(xbox.buttons[7].value);
 
+                // Calculate the new heading and tilt.
+                var heading = camera.heading + rx * ANGULAR_RATIO;
+                var tilt = camera.tilt - ry * ANGULAR_RATIO;
+
+                // Calculate the z-dependant linear ratio.
+                var speed   = camera.position.z * LINEAR_RATIO;
+
+                // Calculate the x, y and z.
                 var x =
                     camera.position.x +
                     speed * lx * Math.cos(camera.heading * Math.PI / 180) +
@@ -68,10 +122,11 @@
                     camera.position.y +
                     speed * lx * Math.sin(-camera.heading * Math.PI / 180) -
                     speed * ly * Math.cos(camera.heading * Math.PI / 180);
-                var z = camera.position.z;
-                if (lp) { z -= speed; }
-                if (rp) { z += speed; }
+                var z = camera.position.z +
+                    speed * -lt +
+                    speed * rt;
 
+                // Assign an autocast camera using the heading, tilt and position calculated above.
                 view.camera = {
                     fov: 55,
                     heading: heading,
@@ -86,19 +141,9 @@
                     tilt: tilt
                 };
             }
-        }
 
-        //function getGamepad() {
-        //    // http://luser.github.io/gamepadtest/
-        //    // https://github.com/luser/gamepadtest
-        //    // https://developer.mozilla.org/en-US/docs/Web/API/Gamepad
-        //    var gamepads = navigator.getGamepads();
-        //    if (!gamepads) {
-        //        return null;
-        //    }
-        //    if (gamepads.length === 0) {
-        //        return null;
-        //    }
-        //    return gamepads[0];
-        //}
-    });
+            // Lastly request that this function be re-run before the next re-paint.
+            window.requestAnimationFrame(gameloop);
+        }
+    }
+);
